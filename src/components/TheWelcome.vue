2a7c34519854592f7324265bbe8b50624c8cf324
anthonysaab1 <152026821+anthonysaab1@users.noreply.github.com>
2024-09-07 19:16:36 +0300

    Add files via upload

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, watchEffect, computed } from 'vue'
+let searchBar = ref('')
+let productList = ref('')
+let ListOfProducts = ref('')
+watchEffect(() => {
+  productList.value = computed(() => {
+    return ListOfProducts.value.filter((item) => item.name.toLowerCase().includes(searchBar.value))
+  })
+})
+</script>
+
+<template>
+  <h1>hello from anthony</h1>
+  <input type="text" placeholder="Search" v-model="searchBar" @input="searchValue" />
+</template>
